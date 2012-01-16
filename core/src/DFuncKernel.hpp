8127a3f30e678d4abd8c6bd3fc5238cb4a8c1d12
Stuart Slattery <sslattery617@gmail.com>
2012-01-15 20:21:49 -0700

    Added the distribution kernel interface. Nothing there yet but looking at phalanx as implementation.

---
+++ core/src/DFuncKernel.hpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------//
+// \file DFuncKernel.hpp
+// \author
+// \brief Protocol definition for distribution function kernels.
+//---------------------------------------------------------------------------//
+
+#ifndef FOOD_DFUNCKERNEL_HPP
+#define FOOD_DFUNCKERNEL_HPP
+
+namespace FOOD 
+{
+
+class DFuncKernel
+{
+
+};
+
+}
+
+#endif // end FOOD_DFUNCKERNEL_HPP
+
+//---------------------------------------------------------------------------//
+// end DFuncKernel.hpp
+//---------------------------------------------------------------------------//
+
