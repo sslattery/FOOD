291de30d53faa62e46aecc0936a72a7ef694e888
Stuart Slattery <sslattery617@gmail.com>
2012-02-16 08:49:55 -0600

    Putting FOOD under the LGPL. See LICENSE for more details

--- core/src/BasisFactory_Def.hpp
+++ core/src/BasisFactory_Def.hpp
@@ -1,4 +1,9 @@
 //---------------------------------------------------------------------------//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+//
 // \file BasisFactory_Def.hpp
 // \author Stuart Slattery
 // \brief Factory method defintion for basis generation.
--- core/src/TensorTemplate.hpp
+++ core/src/TensorTemplate.hpp
@@ -1,4 +1,9 @@
 //---------------------------------------------------------------------------//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+//
 // \file TensorTemplate.hpp
 // \author Stuart Slattery
 // \brief Tensor template definition.
